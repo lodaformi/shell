9c8436d0df582237d51af9a3d7ecb580b11e3f02
lodaformi <15073071+lodaformi@users.noreply.github.com>
2021-06-29 11:31:16 +0800

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo
+echo -e "\t\t***系统管理工具***\n\n"
+
+echo -e "\t1. 显示磁盘空间信息"
+echo -e "\t2. 显示网络接口信息"
+echo -e "\t3. 显示内存使用信息"
+echo -e "\t0. 退出菜单\n"
+
+echo -en "\t\t输入选项: "
+read -n1 choice
+
+echo -e "\n你的选择是: " $choice
