9b1242d81d62b6f1fc6f43d648b3a6d81d78c41d
lodaformi <15073071+lodaformi@users.noreply.github.com>
2021-06-29 11:36:27 +0800

    Update yum_auto.sh

--- yum_auto.sh
+++ yum_auto.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#脚本接受一个参数挂载点的绝对路径，挂载点如果不存在需要创建
+#备份/etc/yum.repos.d/目录下的其他配置文件
+#最后显示yum is ok，不能显示多余的正确或错误提示信息
+
 #使用位置变量接受挂载点
 if (($# < 1)) #判断位置参数的个数$#，不能小于1
 then
@@ -10,7 +14,7 @@ fi
 mount -o loop /root/CentOS-6.4-x86_64-bin-DVD1.iso $1 &> /dev/null
 
 #如果路径不存在，则创建
-[ -e $1 ] || mkdir -p $1
+[[ -e $1 ]]| mkdir -p $1
 
 #备份原始的yum配置文件
 (cd /etc/yum.repos.d ; rename .repo .repo.bak *.repo &> /dev/null)
