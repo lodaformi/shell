c6ec7609017aeb186d69530847091b538e42872a
lodaformi <15073071+lodaformi@users.noreply.github.com>
2021-06-30 15:26:54 +0800

    Update select_balls.sh

--- select_balls.sh
+++ select_balls.sh
@@ -11,7 +11,7 @@ res_blue=()
 echo -e "\033[31mred balls: ${red_balls[@]}\033[0m"   
 echo -e "\033[34mblue balls: ${blue_balls[@]}\033[0m"
 
-read -n1 -p "press any key to start." key
+read -n1 -p "按任意键随机选一注" key
 
 #select 6 red balls using for loop
 for i in $(seq 1 6)
@@ -22,9 +22,10 @@ do
         res=${red_balls[$rd_num]} #将随机数下标对应的值选出
         res_red[$i]=$res       #将该值赋给res_red数组
         unset red_balls[$rd_num] #将该下标位置的数据释放
-
-        red_balls=($(echo ${red_balls[@]}))  #将元素少一个的red_balls数组重新赋值给自身
-        #注意写法
+        
+        #重点，注意写法
+        red_balls=(${red_balls[@]})  #将元素少一个的red_balls数组重新赋值给自身
+        
         
         #这时另外一种更新数组自身的思路，但实现上不太好
         #会出现这种结果，令人费解，17 red_balls[23] 15 04 red_balls[18]
