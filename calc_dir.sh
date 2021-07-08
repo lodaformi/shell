8eec90bdd405223eb9f685d8b2062458fb0a0c8d
lodaformi <15073071+lodaformi@users.noreply.github.com>
2021-07-08 21:24:23 +0800

    Update calc_dir.sh

--- calc_dir.sh
+++ calc_dir.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+#$1代表位置参数，接收执行程序的第一个参数
 num=$(find $1 -type d |tail -n +2 | wc -l)
 echo "Thu number of directory: $num"
 
-echo "\$0 is $0"
-echo "\$# is $#"
+echo "\$0 is $0"  #$0表示程序本身
+echo "\$# is $#"  #$#表示参数的总个数
