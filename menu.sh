c0a8ff2fa52ab722569e21919410bc528bef30ce
lodaformi <15073071+lodaformi@users.noreply.github.com>
2021-07-08 21:32:22 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -8,7 +8,7 @@ echo -e "\t2. 显示网络接口信息"
 echo -e "\t3. 显示内存使用信息"
 echo -e "\t0. 退出菜单\n"
 
-echo -en "\t\t输入选项: "
+echo -en "\t\t输入选项: "   #echo -n表示不换行
 read -n1 choice
 
 echo -e "\n你的选择是: " $choice
