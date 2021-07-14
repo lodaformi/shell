fb77e1d7eb13a8fb3b5a9f39349182370e17ef6e
lodaformi <15073071+lodaformi@users.noreply.github.com>
2021-07-14 17:05:10 +0800

    Update analyse_user_v2.sh

--- branch_and_loop/analyse_user_v2.sh
+++ branch_and_loop/analyse_user_v2.sh
@@ -8,13 +8,15 @@ system=0
 normal=0
 while read USER  ID
 do
-	if (($ID>=500 )) && (($ID<=60000))
+	if (($ID>=500 )) && (($ID<=60000))	#普通用户的uid大于等于500，小于等于60000
 	then
 		echo "$USER是普通用户-$ID"
 		((normal+=1))
-	else
-		echo "$USER是系统用户-$ID"
-		((system+=1))
+		#echo "normal is $normal"	
+	else 					#系统用户uid小于500或大于60000
+		echo "$USER是系统用户-$ID"		
+		((system+=1))	
+		#echo "system is $system"
 	fi
 done < user_id.txt   #从文件中读取数据
 
