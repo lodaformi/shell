be39a668f64068e530eaa043dda7430c99301b80
lodaformi <15073071+lodaformi@users.noreply.github.com>
2021-07-15 23:22:22 +0800

    Update analyse_user_v1.sh

--- branch_and_loop/analyse_user_v1.sh
+++ branch_and_loop/analyse_user_v1.sh
@@ -7,14 +7,14 @@
 system=0
 normal=0
 
-#这种方式不知道为什么system和normal变量不会自增，请用v2版本
+#通过调试信息可以看出循环内的变量在自增，但while循环 外 的变量没有变化，即使用export变成全局变量也不行
 cat passwd | cut -d: -f1,3| tr ":" " " | while read USER  ID
 do
 	if (($ID<500 )) || (($ID>60000))
 	then
 		echo "$USER是系统用户-$ID"
 		((system+=1))
-		echo "system is $system"	#通过调试信息可以看出变量在自增，但while循环外的变量没有变化，即使用export变成全局变量也不行
+		echo "system is $system"	
 	else
 		echo "$USER是普通用户-$ID"
 		((normal+=1))
